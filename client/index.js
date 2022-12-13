657158095c5a4edd7d400eab8df150a82fb99af3
Serge Mastaki <serge.mastaki@gmail.com>
2022-12-13 18:07:03 +0200

    [ADD] Prover and verifier

--- client/index.js
+++ client/index.js
@@ -4,11 +4,16 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+const merkleTree = new MerkleTree(niceList);
+
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+
+  const name = 'Norman Block';
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name, proof
   });
 
   console.log({ gift });
